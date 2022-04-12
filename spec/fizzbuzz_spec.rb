69ec272161a18f239c595db56f5a04fe9154e58b
Erlantz <erlantzram@gmail.com>
2022-04-12 15:56:29 +0100

    2 tests added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,10 @@ describe 'fizzbuzz' do
   it 'it returns fizz when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'it returns buzz when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'it returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
 end
