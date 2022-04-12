a68f27387b55c30f6f4a4c21e611dd5b5d6a4801
sjtinsley <simonjtinsley@gmail.com>
2022-04-12 15:58:00 +0100

    Write final set of tests for the spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,10 @@ describe 'fizzbuzz' do
   it 'it returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
   end
+  it 'it returns fizzbuzz when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  it 'it returns 13 when passed 13' do
+    expect(fizzbuzz(13)).to eq 13
+  end
 end
